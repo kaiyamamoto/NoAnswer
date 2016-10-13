37a58060753e31113437d35441abccfcb331351e
TSITCL\s162196 <dqx_2527@yahoo.co.jp>
2016-10-13 19:21:48 +0900

    up

---
+++ Assets/Scripts/playManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playManager : MonoBehaviour
+{
+    public GameObject soul_prefab;
+
+    public float waitingTime = 2.0f;
+	
+	// Update is called once per frame
+	void Awake ()
+    {
+        InvokeRepeating("Create", waitingTime, waitingTime);
+    }
+
+    void Create()
+    {
+        Instantiate(soul_prefab,new Vector3(Random.Range(-2.0f,2.0f),-5.5f), Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/soulScripts.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class soulScripts : MonoBehaviour
+{
+    // 移動スピード
+    private float speed_x;
+    private float speed_y;
+
+    void Awake()
+    {
+        // 速度設定
+        speed_x = Random.Range(-10.0f, 10.0f);
+        speed_y = Random.Range(5.0f, 10.0f);
+    }
+
+    void Update()
+    {
+
+        // 移動
+        Move();
+
+        GetComponent<Rigidbody2D>().WakeUp();
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag=="Screen")
+        {
+            Debug.Log("return");
+
+            speed_x *= -1.0f;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag=="Haeven")
+        {
+            Debug.Log("soul arrival");
+
+            Destroy(this.gameObject);
+        }
+    }
+
+    // 移動
+    public void Move()
+    {
+        this.transform.position += new Vector3(speed_x * Time.deltaTime, speed_y * Time.deltaTime, 0.0f);
+    }
+}
